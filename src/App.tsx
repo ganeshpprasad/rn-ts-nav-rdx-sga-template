edcb98547b17f7c7f1ff0fdaa879a5cdf17d8034
ganeshp <ganeshpprasad@gmail.com>
2020-03-28 20:22:23 +0530

    move app inside src

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+/**
+ *
+ * App with entry logic
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+declare var global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Hello auth logic</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
